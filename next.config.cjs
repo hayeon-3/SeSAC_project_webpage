626c33b5bef24091c5945c13d12a7e37047e62f5
hayeon-3 <qkrgkdus1031@gmail.com>
2025-08-22 14:18:26 +0900

    pages,cjs

---
+++ next.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    reactStrictMode: true, // React Strict Mode 활성화 (옵션)
+    swcMinify: true, // SWC를 사용한 빌드 최적화
+  };
+
+  
--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    reactStrictMode: true, // React Strict Mode 활성화 (옵션)
-    swcMinify: true, // SWC를 사용한 빌드 최적화
-  };
-
-  
